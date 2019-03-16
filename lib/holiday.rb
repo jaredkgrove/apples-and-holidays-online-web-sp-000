ea2c94a609eaf8d5fa0ab28f477834e3108a27ad
Jared Grove <jaredkgrove@gmail.com>
2019-03-16 22:37:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash.values.values.flatten
+  binding.pry
 end
 
 def all_holidays_with_bbq(holiday_hash)
